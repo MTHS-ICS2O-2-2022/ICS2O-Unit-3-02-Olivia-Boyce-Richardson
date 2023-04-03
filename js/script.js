1007fbeeb459a1f57470b7c2f9f849fc54024c52
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-04-03 17:29:56 +0000

    html work

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const height = parseInt(document.getElementById('height of the triangle').value)
+  const base = parseInt(document.getElementById('base of the triangle').value)
+
+  // process
+  const area = height / 2 * base
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
